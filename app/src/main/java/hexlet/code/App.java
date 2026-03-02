0eaee0ac408e6f196dcf649ac72d0033624a4929
Demidov Ivan <ivan.demidov.01@list.ru>
2026-03-03 01:15:46 +0300

    add command greet

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,21 @@
 package hexlet.code;
 
+import java.util.Scanner;
+
 class App {
     public static void main(String[] args) {
-        Cli.hello();
+        System.out.print("""
+                Please enter the game number and press Enter.
+                1 - Greet
+                0 - Exit
+                Your choice:\s""");
+
+        Scanner scanner = new Scanner(System.in);
+        int choice = scanner.nextInt();
+
+        if (choice == 1) {
+            System.out.println();
+            Cli.hello();
+        }
     }
 }
