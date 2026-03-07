0669c375150bf1cf5720c0b8c372f55034f7d4cb
Demidov Ivan <ivan.demidov.01@list.ru>
2026-03-07 15:36:00 +0300

    change names

--- app/src/main/java/hexlet/code/games/EvenGame.java
+++ app/src/main/java/hexlet/code/games/EvenGame.java
@@ -9,18 +9,18 @@ public final class EvenGame {
     }
 
     public static void run(Scanner scanner) {
-        final int maxNumber = 100;
-        final int roundsCount = 3;
+        final int limit = 100;
+        final int maxRounds = 3;
         final int pairSize = 2;
-        final int questionIndex = 0;
-        final int answerIndex = 1;
+        final int question = 0;
+        final int answer = 1;
         final String rule = "Answer 'yes' if the number is even, otherwise answer 'no'.";
 
-        String[][] rounds = new String[roundsCount][pairSize];
-        for (int round = 0; round < roundsCount; round++) {
-            int number = RandomUtils.nextInt(maxNumber);
-            rounds[round][questionIndex] = String.valueOf(number);
-            rounds[round][answerIndex] = number % 2 == 0 ? "yes" : "no";
+        String[][] rounds = new String[maxRounds][pairSize];
+        for (int round = 0; round < maxRounds; round++) {
+            int number = RandomUtils.nextInt(limit);
+            rounds[round][question] = String.valueOf(number);
+            rounds[round][answer] = number % 2 == 0 ? "yes" : "no";
         }
 
         Engine.run(rule, rounds, scanner);
